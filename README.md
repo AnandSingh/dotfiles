a624ab504deb3553fcaa8ba833fc4c2e36c17096
Anand Singh <anandsingh@fb.com>
2022-10-04 13:09:46 -0700

    added initial files

---
+++ README.md
@@ -0,0 +1,14 @@
+# setup tmux, zsh, and vim
+
+There are three aspects to this setup:
+- Edit/navigate and run code in the same window simultaneously using
+  [Tmux](https://github.com/tmux/tmux/wiki).
+- Develop code with [Vim](https://www.vim.org/), configured with popular
+  plugins and sensible defaults.
+- Enable directory navigation and display git branch/status with [Oh My
+  Zsh](https://ohmyz.sh/).
+
+## Getting started
+  ```
+./setup.sh
+  ```
---
+++ setup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -e -x
+
+local DIR=${HOME}
+
+get_dotfiles () {
+    echo "(1/4): GETTING DOTFILES..."
+    git clone https://github.com/AnandSingh/dotfiles.git $DIR/dotfiles
+    ln -s $DIR/dotfiles/.tmux.conf $DIR/.tmux.conf
+    ln -s $DIR/dotfiles/.vimrc $DIR/.vimrc
+}
+
+setup_vim () {
+
+    echo "(2/4) SETTING UP VIM..."
+    # Install black for formatting
+    pip3 install black
+
+    # Install vim plug for package management
+    curl -fLo $DIR/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    # Install packages
+    vim +PlugInstall +qall
+
+}
+
+setup_tmux () {
+
+    echo "(3/4) SETTING UP TMUX..."
+    # Install tmux dependencies
+    sudo dnf -y install ncurses-devel
+    sudo dnf -y install libevent-devel
+
+    # Get the latest version
+    git clone https://github.com/tmux/tmux.git
+    cd tmux
+    sh autogen.sh
+    ./configure && make install
+    cd ..
+    # Install htop
+    sudo dnf -y install htop
+}
+
+setup_zsh () {
+
+    echo "(4/4) SETTING UP ZSH..."
+    sudo dnf -y update && sudo dnf -y install zsh
+    # Install oh-my-zsh
+    wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O $DIR/install.sh
+    cd $DIR
+    echo pwd
+    ./install.sh
+    # Change the default shell to zsh
+    sudo dnf -y install util-linux-user
+    chsh -s /bin/zsh ${USER} 
+}
+
+get_dotfiles
+setup_vim
+setup_tmux
+setup_zsh
