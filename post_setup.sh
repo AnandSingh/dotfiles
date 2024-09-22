abe158faabad7ac130a90d57c517662b50aabb7b
Tinkering Turtle <mail.tinkering.turtle@gmail.com>
2024-09-22 10:19:46 -0700

    post setup

--- post_setup.sh
+++ post_setup.sh
@@ -3,3 +3,8 @@
 sudo dnf install -y blackbox-terminal
 exec zsh
 p10k configure
+
+sudo dnf install -y fzf bat navi cheat peco nmap inotify-tools
+
+wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh -O /usr/local/share/zsh/site-functions/_tmuxinator
+
